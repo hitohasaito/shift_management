0ca3893e67333077222973caadefc4f68534de1d
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-14 20:30:04 +0900

    【Add】勤務希望登録機能を作成

--- app/controllers/request_shifts_controller.rb
+++ app/controllers/request_shifts_controller.rb
@@ -1,2 +1,20 @@
 class RequestShiftsController < ApplicationController
+  def new
+    @request = RequestShift.new
+  end
+
+  def create
+    @request = RequestShift.new(request_params)
+    if @request.save
+      redirect_to "request_shifts_path"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request_shift).permit(:worked_on, :worked_at, work_job)
+  end
 end
