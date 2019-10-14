a1287012366c10f31d689b8b6a0ffb996b1a47d1
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-14 18:50:18 +0900

    【Add】シフト作成後一覧画面に変移する機能を実装

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -3,4 +3,23 @@ class ShiftsController < ApplicationController
   def new
     @shift = Shift.new
   end
+
+  def create
+    @shift = Shift.new(shift_params)
+    if @shift.save
+      redirect_to shifts_path
+    else
+      render "new"
+    end
+  end
+
+  def index
+    @shifts = Shift.all
+  end
+
+  private
+
+  def shift_params
+    params.require(:shift).permit(:duty_on, :duty_at, :job, :assigned_user)
+  end
 end
