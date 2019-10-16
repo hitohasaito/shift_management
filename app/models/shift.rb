6e65cd35a8b6f53001116275d0f5870999d34823
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-16 20:00:03 +0900

    【Add】shiftモデルとuserモデル間にアソシエーションを追加

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,6 @@
 class Shift < ApplicationRecord
+  has_many :assigned_works
+  has_many :users, through :assigned_works, source: :user
 
   validates :duty_on, presence: true
   validates :duty_at, presence: true
