de0eb964e216a0d874c080d69ffd982d315348b4
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-21 23:50:16 +0900

    【Fix】シフトの条件に合致するアルバイトの検索機能を修正

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -23,12 +23,16 @@ class ShiftsController < ApplicationController
       @shift = Shift.find(params[:id])
 
       #勤務日にマッチしている人たちのuser_idをゲット
+      # day_match_users = shift.match_days(@requests, shift)
+
       days = @requests.pluck(:worked_on)
       find_day = days.grep(@shift.duty_on)
       match_request_days = RequestShift.where(worked_on: find_day)
       day_match_users = match_request_days.pluck(:user_id, :created_at)
 
+
       #勤務時間にマッチしている人たちのuser_idをゲット
+      # match_time_users = shift.match_times(shift)
       match_request_times = RequestShift.where("start_work_at =< ? ", @shift.started_at) && RequestShift.where("end_work_at >= ? ", @shift.end_at)
       match_time_users = match_request_times.pluck(:user_id, :created_at)
 
@@ -36,20 +40,20 @@ class ShiftsController < ApplicationController
       do_works = @requests.pluck(:work_job)
       find_works = do_works.grep(@shift.job)
       match_request_jobs = RequestShift.where(work_job: find_works)
-      job_match_users = match_request_jobs.pluck(:user_id, :created_at)
+      job_match_users = match_request_jobs.pluck(:user_id,:created_at)
 
       #3つの配列で重複しているuser_idをゲット
-      match_users = day_match_users && match_time_users && job_match_users
+      match_users = [day_match_users, match_time_users, job_match_users].inject(&:&)
+      binding.pry
 
       if match_users.present?
         sort_user = match_users.sort_by{|a, b| b }.first
         match_user_id = sort_user.first
 
-        @shift.assigned_user = User.find(match_user_id).name
-        @shift.save
-        
         assign = @shift.assigned_works.build(user_id: match_user_id, shift_id: @shift.id)
         assign.save
+        @shift.assigned_user = assign.user.name
+        @shift.save
       else
         flash[:notice] = "合致する志望者がいません"
       end
@@ -85,7 +89,7 @@ class ShiftsController < ApplicationController
   private
 
   def shift_params
-    params.require(:shift).permit(:duty_on, :started_at, :end_at, :job, :assigned_user)
+    params.require(:shift).permit(:duty_on, :started_at, :end_at, :job, :assigned_user )
   end
 
   def get_id
