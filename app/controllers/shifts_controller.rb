987f90a111cd7e24f2db2b78a2bcd556b22d728a
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-22 00:06:37 +0900

    【Remove】コメントや不要な空行を削除

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -20,32 +20,15 @@ class ShiftsController < ApplicationController
     @requests = RequestShift.all
 
     if params[:id]
-      @shift = Shift.find(params[:id])
-
+      @shift = Shift.find(params[:id])¥
       #勤務日にマッチしている人たちのuser_idをゲット
       day_match_users = @shift.match_days(@requests, @shift)
-
-      # days = @requests.pluck(:worked_on)
-      # find_day = days.grep(@shift.duty_on)
-      # match_request_days = RequestShift.where(worked_on: find_day)
-      # day_match_users = match_request_days.pluck(:user_id, :created_at)
-
-
       #勤務時間にマッチしている人たちのuser_idをゲット
       match_time_users = @shift.match_times(@shift)
-      # match_request_times = RequestShift.where("start_work_at =< ? ", @shift.started_at) && RequestShift.where("end_work_at >= ? ", @shift.end_at)
-      # match_time_users = match_request_times.pluck(:user_id, :created_at)
-
       #勤務業務にマッチしている人たちのuser_idをゲット
       job_match_users = @shift.match_jobs(@requests, @shift)
-      # do_works = @requests.pluck(:work_job)
-      # find_works = do_works.grep(@shift.job)
-      # match_request_jobs = RequestShift.where(work_job: find_works)
-      # job_match_users = match_request_jobs.pluck(:user_id,:created_at)
-
       #3つの配列で重複しているuser_idをゲット
       match_users = [day_match_users, match_time_users, job_match_users].inject(&:&)
-
       if match_users.present?
         sort_user = match_users.sort_by{|a, b| b }.first
         match_user_id = sort_user.first
@@ -63,9 +46,6 @@ class ShiftsController < ApplicationController
         @requests = RequestShift.all
     end
   end
-
-  # 17:00-21:00　シフト
-  # 13:00-22:00  希望
 
   def show
   end
