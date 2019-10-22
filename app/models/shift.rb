a02277f208d59e5620e270045823e95740be0ac0
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-22 21:05:17 +0900

    【Fix】時間の照合機能を修正

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -8,28 +8,31 @@ class Shift < ApplicationRecord
   validates :end_at, presence: true
   validates :job, presence: true
 
-
-
-  # def work_day
-  #   shift = Shift.find(params[:id])
-  #   request = RequestShift.all
-
   def match_days(requests, shift)
     days = requests.pluck(:worked_on)
     find_day = days.grep(shift.duty_on)
     match_request_days = RequestShift.where(worked_on: find_day)
-    day_match_users = match_request_days.pluck(:user_id, :created_at)
+    day_match_users = match_request_days.pluck(:id)
   end
 
   def match_times(shift)
-    match_request_times = RequestShift.where("start_work_at =< ? ", shift.started_at) && RequestShift.where("end_work_at >= ? ", shift.end_at)
-    match_time_users = match_request_times.pluck(:user_id, :created_at)
+    match_request_times = RequestShift.where("start_work_at <= ? ", shift.started_at).where("end_work_at >= ? ", shift.end_at)
+    match_time_users = match_request_times.pluck(:id)
   end
-
+#勤務希望開始時間よりも、シフト開始時間の方が遅い
   def match_jobs(requests, shift)
     do_works = requests.pluck(:work_job)
     find_works = do_works.grep(shift.job)
     match_request_jobs = RequestShift.where(work_job: find_works)
-    job_match_users = match_request_jobs.pluck(:user_id, :created_at)
+    job_match_users = match_request_jobs.pluck(:id)
+  end
+
+  # def find_match_user(match_records)
+  #   match_columns = match_records.pluck(:user_id, :created_at)
+  #   sort_user = match_columns.sort_by{|a, b| b }.first
+  #   match_user_id = sort_user.first
+  # end
+
+  def time_scope
   end
 end
