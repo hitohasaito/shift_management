c1f59a69df468387b8b0034eeae9e3f729dc036d
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-23 21:51:03 +0900

    【Add】rails gでコントローラーを作り直して再度コメント投稿機能を実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def create
+    @shift = Shift.find(params[:shift_id])
+    @comment = @shift.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    respond_to do |format|
+      binding.pry
+      if @comment.save
+        format.html{ redirect_to shift_path(@shift)}
+      else
+        format.html{ redirect_to shift_path(@shift), notice: "投稿に失敗しました"}
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :shift_id)
+  end
+end
