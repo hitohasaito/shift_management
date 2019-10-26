b0de22b15f05d44d939084035b6dcff92d17aa4b
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-26 22:21:04 +0900

    【Add】パスワードなしでユーザー情報編集できるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :check_user,only:[:destroy,:index]
   before_action :set_id, only:[:destroy, :edit, :update]
+  before_action :allow_without_password, only: [:update]
 
   def index
     @users = User.all
@@ -15,14 +16,22 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user.update(user_params)
+    if @user.update(user_params)
     redirect_to users_path, notice:"更新しました"
+end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :number, :password, :password_confirmation, :admin)
+    params.require(:user).permit(:name, :email, :number, :password, :password_confirmation, :admin, :current_user)
+  end
+
+  def allow_without_password
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+          params[:user].delete(:password)
+          params[:user].delete(:password_confirmation)
+      end
   end
 
   def check_user
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
   }
 
-  resources :users, only: [:index, :edit, :update]
+resources :users, only: [:index, :edit, :update]
 
   resources :shifts do
     resources :comments
