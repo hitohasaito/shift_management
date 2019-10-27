f2e3803e15a0e4f3a4bd4e47297ed4f6479d0d08
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-27 18:42:30 +0900

    【Add】日時と時間の表示を日、時、分のような表記に変更

---
+++ spec/factories/request_shift.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+  factory :request_shift do
+    worked_on { Time.zone.local(2019, 10, 10)   }
+    start_work_at { DateTime.new(2019, 10 ,10, 10, 00) }
+    end_work_at { DateTime.new(2019, 10 ,10, 15, 00) }
+    work_job { "自習室" }
+  end
+end
