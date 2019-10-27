69f0e580797bff21309ae61d98b81d8d2a7e2ef3
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-27 21:00:47 +0900

    【Add】シフト作成のテストを途中まで実装

--- spec/features/shift.spec.rb
+++ spec/features/shift.spec.rb
@@ -5,8 +5,7 @@ RSpec.feature "シフト情報管理機能", type: :feature do
   background do
     @user = FactoryBot.create(:user)
     #管理者ログイン
-
-    # @shift = FactoryBot.create(:shift)
+    @shift = FactoryBot.create(:shift)
     # @request = FactoryBot.create(:request_shift, user_id: @user.id)
 
     visit new_user_session_path
@@ -17,14 +16,26 @@ RSpec.feature "シフト情報管理機能", type: :feature do
     # fill_in "user[email]", with: "name1@email.com"
     # fill_in "user[email]", with: "password1"
 
-    click_link 'ログイン'
+    click_button 'ログイン'
 
     expect(page).to have_content "ようこそ！"
   end
 
   scenario "シフト作成のテスト" do
-  visit new_shift_path
+    visit new_shift_path
+#select "ラベル", from: "セレクトメニューのid"
 
+    fill_in "shift_duty_on", with:"2019,10,28"
+    # save_and_open_page
+    select 10, from: "shift_started_at_4i"
+    select 10, from: "shift_started_at_5i"
+    select 12, from: "shift_end_at_4i"
+    select 30, from: "shift_end_at_5i"
+    # byebug
+    # expect(page).to have_content "2019年10月28日"
+    expect(page).to have_content "2019-10-28"
+    expect(page).to have_content "10時10分"
+    expect(page).to have_content "12時30分"
   end
   #
   # scenario "シフト一覧のテスト" do
