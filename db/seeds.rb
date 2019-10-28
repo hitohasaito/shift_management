9b9509868b654a7cb68f0eeece10de5518d51027
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-29 01:37:33 +0900

    【Fix】バリデーションを追加したらseedデータでエラーが出たので修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,23 +26,23 @@ end
 
 30.times do |n|
 
-  from = Time.parse("2019/10/01")
-  to = Time.parse("2019/10/31")
+  from = Time.parse("2019/11/01")
+  to = Time.parse("2019/11/31")
   date = Random.rand(from..to)
 
   from = Time.parse("9:00:00")
   to = Time.parse("13:00:00")
-  time = Random.rand(from..to)
+  start_time = Random.rand(from..to)
 
   from = Time.parse("13:30:00")
   to = Time.parse("22:00:00")
-  time = Random.rand(from..to)
+  end_time = Random.rand(from..to)
 
   job = "受付"
 
   Shift.create!(duty_on: date,
                 started_at: time,
-                end_at: time,
+                end_at: end_time,
                 job: job,
                 status: 1
                )
@@ -50,13 +50,17 @@ end
 
 30.times do |n|
 
-  from = Time.parse("2019/10/01")
-  to = Time.parse("2019/10/31")
+  from = Time.parse("2019/11/01")
+  to = Time.parse("2019/11/31")
   date = Random.rand(from..to)
 
   from = Time.parse("9:00:00")
+  to = Time.parse("13:00:00")
+  start_time = Random.rand(from..to)
+
+  from = Time.parse("13:30:00")
   to = Time.parse("23:00:00")
-  time = Random.rand(from..to)
+  end_time = Random.rand(from..to)
 
   job = "自習室"
 
