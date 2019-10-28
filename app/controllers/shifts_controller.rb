74e21984b2e4102bb72f8fe78964b988df97ba56
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-29 08:21:29 +0900

    【Fix】一覧画面で日付順になるように修正

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -19,49 +19,42 @@ class ShiftsController < ApplicationController
   end
 
   def index
-    @shift = Shift.new
-
+    # @shift = Shift.new
+    @requests = RequestShift.all
     @q = Shift.ransack(params[:q])
-    @shifts = @q.result(distinct: :true)
+    @shifts = @q.result(distinct: :true).order(duty_on: :asc)
 
     if params[:id]
       @requests = RequestShift.all
       @shift = Shift.find(params[:id])
-      #勤務日にマッチしている人たちのuser_idと登録日時をゲット
+
       day_match_requests = @shift.match_days(@requests, @shift)
-      #勤務時間にマッチしている人たちのuser_idと登録日時をゲット
       time_match_requests = @shift.match_times(@shift)
-      #勤務業務にマッチしている人たちのuser_idと登録日時をゲット
       job_match_requests = @shift.match_jobs(@requests, @shift)
-      #3つの配列で重複しているuser_idと登録日時をゲット
+      #上記で作られた3つの配列全てで重複しているuser_idをゲット
       match_requests = [day_match_requests, time_match_requests,job_match_requests].inject( &:& )
 
       if match_requests.present?
-
          match_user_id = @shift.find_and_sort_user_id(match_requests)
 
          sum_worktime = @shift.end_at - @shift.started_at
-
          assign = @shift.assigned_works.build(user_id: match_user_id, shift_id: @shift.id, assigned_time:sum_worktime )
+
          @user = User.find( match_user_id)
         #ユーザーが持っているシフトの日付 登録しようとしている日付と同じだったら
          if @user.shifts.pluck(:duty_on).include?(@shift.duty_on)
-           flash[:notice]= "#{@user.name}さんは#{@shift.duty_on}で勤務が登録されているシフトがあります。登録後は重複をチェックしてください"
            @shift.assigned_user = assign.user.name
            @shift.save
            assign.save
+           flash.now[:notice]= "#{@user.name}さんは#{@shift.duty_on}で勤務が登録されているシフトがあります。登録後は重複をチェックしてください"
          else
            @shift.assigned_user = assign.user.name
            @shift.save
            assign.save
          end
       else
-          redirect_to shifts_path, notice: "合致する志望者がいません"
+        redirect_to shifts_path, notice: "合致する志望者がいません"
       end
-
-    else
-      @shift = Shift.all.order(created_at: :asc)
-      @requests = RequestShift.all
     end
   end
 
