50d772c49ba75c0ff0a5d54ed46cb42c577cf8a4
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-30 22:27:16 +0900

    【Add】モデルに定義したメソッドを修正

--- spec/models/request_shift_spec.rb
+++ spec/models/request_shift_spec.rb
@@ -24,4 +24,14 @@ RSpec.describe RequestShift, type: :model do
      request_shift = RequestShift.new(worked_on: '', start_work_at: '10:00',end_work_at: "12:00", work_job: "")
      expect(request_shift).not_to be_valid
    end
+
+   it "createアクションの時、worked_onが過去の日付の場合バリデーションが通らない" do
+      request_shift = RequestShift.new(worked_on: 'Date.today-1', start_work_at: '10:00',end_work_at: "12:00", work_job: "受付")
+      expect(request_shift).not_to be_valid
+    end
+
+    it "end_work_atがstart_work_atよりも早い時間だとバリデーションが通らない" do
+      request_shift = RequestShift.new(worked_on: '2019,10,19', start_work_at: '12:00',end_work_at: "11:00", work_job: "自習室")
+      expect(request_shift).not_to be_valid
+    end
 end
