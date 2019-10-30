e6dcc42d9dd1fabc273c9a6c7d8d04b0740e7655
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-10-30 12:55:51 +0900

    【Fix】deviseの表記を日本語に修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for:users, controllers: {
     registrations: 'users/registrations',
     sessions: "users/sessions",
+    passwords: "users/passwords",
   }
 
 resources :users, only: [:index, :edit, :update, :destroy]
