1f8ea31c0cc72dd4f9fa2f91cba4ef4b92ab6a2a
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2020-04-25 05:33:21 +0900

    【Remove】不要なseedデータのコードを削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,29 +19,3 @@ end
                admin: "false"
                )
 end
-
-10.times do |n|
-
-  from = Date.today
-  to = Date.today+30
-  date = Random.rand(from..to)
-
-  from = Time.parse("9:00")
-  to = Time.parse("13:00")
-  start_time = Random.rand(from..to)
-  #
-  # from = Time.parse("13:30")
-  # to = Time.parse("20:00")
-  # end_time = Random.rand(from..to)
-  end_time = start_time+36000
-
-  job = "受付"
-  second_job = "自習室"
-
-  Shift.create!(duty_on: date,
-                started_at: start_time,
-                end_at: end_time,
-                job: job,
-                status: 1
-               )
-end
