4ea314eace86fc6d5eb025cc02a5e71b4b7785f5
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2020-04-25 04:55:22 +0900

    【add】seedデータ追加のためにコメントアウトを外す

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
-# 1.times do |n|
-#   name = "admin"
-#   email = "admin@email.com"
-#   password = "password"
-#   number = "08011111111"
-#   User.create!(name: name,
-#                email: email,
-#                password: password,
-#                number: number,
-#                admin: "true"
-#                )
-# end
+1.times do |n|
+  name = "admin"
+  email = "admin@email.com"
+  password = "password"
+  number = "08011111111"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               number: number,
+               admin: "true"
+               )
+end
 
-# 10.times do |n|
-#   User.create!(name: "user#{n}",
-#                email: "user#{n}@email.com",
-#                password: "password#{n}",
-#                number: 12345678976,
-#                admin: "false"
-#                )
-# end
+10.times do |n|
+  User.create!(name: "user#{n}",
+               email: "user#{n}@email.com",
+               password: "password#{n}",
+               number: 12345678976,
+               admin: "false"
+               )
+end
 
 10.times do |n|
 
